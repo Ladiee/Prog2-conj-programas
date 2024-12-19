383457767807c8fc04f92e8197657b62b231de30
Ladiee <ladielmateixeira@gmail.com>
2024-12-18 22:30:00 -0300

    update in .class files

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //pkg_calculadora.Calculadora.menu();
         //pkg_raizes_reais.Raizes_reais.tratamento_expre("23x^2+3x+1");
-        pkg_validador_cpf.Menu_cpf.menu_cpf();
+        pkg_conversor.conversor.menu();
     }
 }
