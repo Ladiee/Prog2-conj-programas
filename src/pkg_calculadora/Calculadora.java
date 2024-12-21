3895bdd6e3c0b5aa7b36f3ca8b904defc97eadda
Ladiee <ladielmateixeira@gmail.com>
2024-12-20 23:20:22 -0300

    Update in calculadora

--- src/pkg_calculadora/Calculadora.java
+++ src/pkg_calculadora/Calculadora.java
@@ -3,14 +3,40 @@ package pkg_calculadora;
 import java.util.Scanner;
 
 public class Calculadora {
-    static float n1, n2;
+    static double n1, n2;
 	static int op;
     
+    public static int isInt (){ // Recebe apenas entradas inteiras
+        Scanner in = new Scanner(System.in);
+
+        while(!in.hasNextInt()) { 
+            in.next();
+            System.out.println("Entrada inválida. Insira um inteiro: ");
+        }
+        int n = in.nextInt();
+
+        return n; 
+    }
+
+    public static double isDouble(){ // Recebe apenas entradas de ponto flutuante
+        Scanner in = new Scanner (System.in);
+
+        while(!in.hasNextDouble()) {//Enquanto a entrada não for um double, ela e ignorada
+            in.next();
+            System.out.println("Entrada inválida. Insira um inteiro: ");
+        }
+        double n = in.nextDouble();
+
+        return n; 
+    }
+
     public static void menu(){
-        try(Scanner input = new Scanner(System.in)){
             do { 
+                Operacoes calc = new Operacoes();
+
                 System.out.println("________MENU________");
                 String string = """
+                        0 - Sair da calculadora
                         1 - Soma
                         2 - Subtração
                         3 - Multiplicação
@@ -28,45 +54,45 @@ public class Calculadora {
                         15 - Validador de quadrado perfeito
                         Escolha uma operação: """;
                 System.out.println(string);
-        
-                op = input.nextInt();  
+                
+                op = isInt();  
             
                 if (op == 0) {
-                    System.err.println("Encerrando Programa!");
+                    System.err.println("Encerrando a calculadora!");
 
-                } else if (op>=1 && op<=9) {
+                } else if (op>=1 && op<=9) { //Esse intervalo corresponde aos metodos de dois parametros
                     System.out.println("Informe os valores:");
                     try {
-                        n1 = input.nextFloat();
-                        n2 = input.nextFloat();
+                        n1 = isDouble();
+                        n2 = isDouble();
                         switch (op) {
-                            case 1 -> System.out.println(pkg_calculadora.Operacoes.somar(n1,n2));
-                            case 2 -> System.out.println(pkg_calculadora.Operacoes.subtrair(n1,n2));
-                            case 3 -> System.out.println(pkg_calculadora.Operacoes.multiplicar(n1, n2));
-                            case 4 -> System.out.println(pkg_calculadora.Operacoes.dividir(n1, n2));
-                            case 5 -> System.out.println(pkg_calculadora.Operacoes.Expo(n1,n2));
-                            case 6 -> System.out.println(pkg_calculadora.Operacoes.div_int(n1, n2));
-                            case 7 -> System.out.println(pkg_calculadora.Operacoes.resto_div(n1, n2));
-                            case 8 -> System.out.println(pkg_calculadora.Operacoes.arranjo(n1, n2));
-                            case 9 -> System.out.println(pkg_calculadora.Operacoes.raiz_grau_n(n1, n2));
+                            case 1 -> System.out.println(calc.somar(n1, n2));
+                            case 2 -> System.out.println(calc.subtrair(n1,n2));
+                            case 3 -> System.out.println(calc.multiplicar(n1, n2));
+                            case 4 -> System.out.println(calc.dividir(n1, n2));
+                            case 5 -> System.out.println(calc.Expo(n1,n2));
+                            case 6 -> System.out.println(calc.div_int(n1, n2));
+                            case 7 -> System.out.println(calc.resto_div(n1, n2));
+                            case 8 -> System.out.println(calc.arranjo(n1, n2));
+                            case 9 -> System.out.println(calc.raiz_grau_n(n1, n2));
                         }
                     } catch (ArithmeticException e) {
-                        System.out.println("Algo deu errado! Tente novamente.");
+                        System.out.println("Algo deu errado aritmeticamente! Tente novamente.");
                     } catch (Exception e){
                         System.out.println("Algo deu errado! Tente novamente.");
                     }
 
-                } else if (op>9 && op <=15){
+                } else if (op>9 && op <=15){ //Esse intervalo corresponde aos metodos de um parametro
                     System.out.println("Informe o valor:");
                     try {
-                        n1 = input.nextFloat();
+                        n1 = isDouble();
                         switch(op){
-                            case 10 -> System.out.println(pkg_calculadora.Operacoes.fatorial(n1));
-                            case 11 -> System.out.println(pkg_calculadora.Operacoes.fatorial(n1));
-                            case 12 -> System.out.println(pkg_calculadora.Operacoes.piso(n1));
-                            case 13 -> System.out.println(pkg_calculadora.Operacoes.teto(n1));
-                            case 14 -> System.out.println(pkg_calculadora.Operacoes.valida_primos(n1));
-                            case 15 -> System.out.println(pkg_calculadora.Operacoes.quadrado_perfeito(n1));
+                            case 10 -> System.out.println(calc.fatorial(n1));
+                            case 11 -> System.out.println(calc.fatorial(n1));
+                            case 12 -> System.out.println(calc.piso(n1));
+                            case 13 -> System.out.println(calc.teto(n1));
+                            case 14 -> System.out.println(calc.valida_primos(n1));
+                            case 15 -> System.out.println(calc.quadrado_perfeito(n1));
                         }
                     } catch (Exception e) {
                         System.out.println("Algo deu errado! Tente novamente.");
@@ -74,7 +100,6 @@ public class Calculadora {
                 } else {
                     System.out.println("Opção não encontrada!");
                 }
-            }while (op!=0);
-        } 
+            }while (op!=0); 
     }      
 }
--- src/pkg_calculadora/Operacoes.java
+++ src/pkg_calculadora/Operacoes.java
@@ -1,59 +1,60 @@
+//Esse pacote contem todos os metodos que serão utilizados pela calculadora
 package pkg_calculadora;
 
 public class Operacoes {
-    //public float n1, n2;
+    private double result;
 
-    public static float somar(float n1, float n2){ 
-        float result = n1+n2;
-        return result;
+    public double somar(double n1, double n2){ 
+        this.result = n1+n2;
+        return this.result;
     } 
     
-    public static float subtrair(float n1, float n2){
-        float result = n1-n2;
-        return result;
+    public double subtrair(double n1, double n2){
+        this.result = n1-n2;
+        return this.result;
     }
 
-    public static float multiplicar(float n1, float n2){
-        float result = n1*n2;
-        return result;
+    public double multiplicar(double n1, double n2){
+        this.result = n1*n2;
+        return this.result;
     }
 
-    public static float dividir(float n1, float n2){
-        float result = n1/n2;
-        return result;
+    public double dividir(double n1, double n2){
+        this.result = n1/n2;
+        return this.result;
     }
 
-    public static double piso(float n1){
-        double result = Math.floor(n1);
-        return result;
+    public double piso(double n1){
+        this.result = Math.floor(n1);
+        return this.result;
     }
 
-    public static double teto(float n1){
-        double result = Math.ceil(n1);
-        return result;
+    public double teto(double n1){
+        this.result = Math.ceil(n1);
+        return this.result;
     }
 
-    public static double raiz_grau_n(float n1, float x){
-        double result = Math.pow(n1, 1/x);
-        return result;
+    public double raiz_grau_n(double n1, double x){
+        this.result = Math.pow(n1, 1/x);
+        return this.result;
     }
         
-    public static double Expo(float n, float x){
+    public double Expo(double n, double x){
         double res = Math.pow(n, x);
         return res;
     }
 
-    public static int div_int(float n1, float n2){
+    public int div_int(double n1, double n2){
         double res = (n1/n2);
         return (int) res;
     }
     
-    public static float resto_div(float n1, float n2){
-        float res = n1%n2;
+    public double resto_div(double n1, double n2){
+        double res = n1%n2;
         return res;
     }
     
-    public static double fatorial(float n){
+    public double fatorial(double n){
         double res=1;
         for (int i= (int) n; i>=1; i--){
             res *= i;
@@ -61,7 +62,7 @@ public class Operacoes {
         return res;
     }
     
-    public static boolean valida_primos(float n){
+    public boolean valida_primos(double n){
         for (int i=2; i<n; i++){
             if (n%i==0){
                 return false;
@@ -70,13 +71,13 @@ public class Operacoes {
         return true;
     }
     
-    public static boolean quadrado_perfeito(float n){
+    public boolean quadrado_perfeito(double n){
         double raiz = Math.sqrt(n);
         boolean res = (raiz*raiz == n);
         return res;
     }
 
-    public static double arranjo (float n, float p){
+    public double arranjo (double n, double p){
         double res = (fatorial(n)/(fatorial(n-p)));
         return res;
     }
