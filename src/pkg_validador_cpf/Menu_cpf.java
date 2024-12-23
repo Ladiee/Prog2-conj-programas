dc4a5bc77a61f5540a9d1d682ad6e3b20aff971a
deLoretoMelo <meloloretomarcelo@gmail.com>
2024-12-22 22:17:21 -0300

    consertado erro de noSuchValue na parte de validação de cpf

--- src/pkg_validador_cpf/Menu_cpf.java
+++ src/pkg_validador_cpf/Menu_cpf.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class Menu_cpf {
 	private static int para;
-	public static void menu_cpf(){
-		try(Scanner input = new Scanner(System.in)){
-			do{
-				System.out.print("Olá! Digite seu cpf para validação: ");
-				Scanner cpf_input = new Scanner(System.in);
-				String cpf_string = cpf_input.next();
-				Valida_cpf cpf = new Valida_cpf(cpf_string);
-				cpf.regex_cpf(cpf.getCpf());
-				System.out.print("Deseja validar um novo cpf? 1-sim, 2-não: ");
-				try {
-					para = input.nextInt();
 
-				} catch (Exception e){
-					System.out.println("valor inválido, tente novamente");
-				} 
-			}while(para != 2);
+	public static void menu_cpf() {
+		do {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Olá! Digite seu cpf para validação: ");
+			Scanner cpf_input = new Scanner(System.in);
+			String cpf_string = cpf_input.next();
+			Valida_cpf cpf = new Valida_cpf(cpf_string);
+			cpf.regex_cpf(cpf.getCpf());
+			System.out.print("Deseja validar um novo cpf? 1-sim, 2-não: ");
+			try {
+				para = input.nextInt();
+
+			} catch (Exception e) {
+				System.out.println("valor inválido, tente novamente");
+			}
+		} while (para != 2);
+
 	}
-}
 }
